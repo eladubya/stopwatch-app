fcf3ee7c1320dac0bcafc3fc8619e8182edb3b6b
Lawrence Wong <lawrencewong@MacBookPro.localdomain>
2026-06-20 09:41:04 -0700

    Add Node.js server for Railway

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'text/plain';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
